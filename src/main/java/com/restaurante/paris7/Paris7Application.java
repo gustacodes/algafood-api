0cd93f0079a204347704570c194d060b5f74a784
Gustavo Alencar <gustavorique@live.com>
2023-06-21 01:41:20 -0300

    Commit inicial

---
+++ src/main/java/com/restaurante/paris7/Paris7Application.java
@@ -0,0 +1,13 @@
+package com.restaurante.paris7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paris7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paris7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurante/paris7/Paris7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.paris7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paris7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
