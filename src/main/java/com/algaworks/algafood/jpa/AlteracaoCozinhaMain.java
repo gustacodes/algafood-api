bed504f118a8ef24178f2efbdbd382df3ed5c739
Gustavo Alencar <gustavorique@live.com>
2023-08-05 01:27:41 -0300

    Atualizando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.Paris7ApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
     public static void main(String[] args) {
         
         ApplicationContext applicationContext = new SpringApplicationBuilder(Paris7ApiApplication.class)
@@ -17,17 +17,10 @@ public class InclusaoCozinhaMain {
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
         Cozinha br = new Cozinha();
+        br.setId(1L);
         br.setNome("Brasileira");
-        
-        br = cadastroCozinha.salvar(br);
 
-        Cozinha jp = new Cozinha();
-        jp.setNome("Japonesa");
-
-        jp = cadastroCozinha.salvar(jp);
-
-        System.out.printf("%d - %s\n", br.getId(), br.getNome());
-        System.out.printf("%d - %s\n", jp.getId(), jp.getNome());
+        cadastroCozinha.salvar(br);
 
     }
 }
