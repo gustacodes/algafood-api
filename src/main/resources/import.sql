ec7fbf648630230943aac4585d6808974511660f
Gustavo Alencar <gustavorique@live.com>
2023-08-05 04:08:50 -0300

    Desafio: mapeando entidades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,22 @@ INSERT INTO cozinha (id, nome) VALUES (2, 'Indiana')
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Gourmet', 10, 1)
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Delivery', 9.50, 1)
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Tuk Tuk Comida', 15, 2)
+
+INSERT INTO forma_pagamento (descricao) VALUES ('Cartao de Credito')
+INSERT INTO forma_pagamento (descricao) VALUES ('Cartao de Debito')
+INSERT INTO forma_pagamento (descricao) VALUES ('Em dinheiro')
+
+INSERT INTO permissao (nome, descricao) VALUES ('Gustavo', 'ACESSO IRESTRITO')
+INSERT INTO permissao (nome, descricao) VALUES ('Pamella', 'ACESSO RESTRITO')
+
+INSERT INTO estado (id, nome) VALUES (1, 'Alagoas')
+
+INSERT INTO cidade (nome, estado_id) VALUES ('Maceio', 1)
+INSERT INTO cidade (nome, estado_id) VALUES ('Arapiraca', 1)
+INSERT INTO cidade (nome, estado_id) VALUES ('Penedo', 1)
+
+INSERT INTO estado (id, nome) VALUES (2, 'Pernambuco')
+
+INSERT INTO cidade (nome, estado_id) VALUES ('Recife', 2)
+INSERT INTO cidade (nome, estado_id) VALUES ('Sertania', 2)
+INSERT INTO cidade (nome, estado_id) VALUES ('Garanhuns', 2)
