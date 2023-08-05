d042238b96d7671e416bfba1d97d03649785b96a
Gustavo Alencar <gustavorique@live.com>
2023-08-05 01:21:10 -0300

    Buscando um objeto pelo id no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.jpa;
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.Paris7ApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class BuscaCozinhaMain {
     public static void main(String[] args) {
         
         ApplicationContext applicationContext = new SpringApplicationBuilder(Paris7ApiApplication.class)
@@ -17,19 +15,10 @@ public class InclusaoCozinhaMain {
         .run(args);
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
 
-        Cozinha br = new Cozinha();
-        br.setNome("Brasileira");
+        System.out.println(cozinha.getNome());
         
-        br = cadastroCozinha.adicionar(br);
-
-        Cozinha jp = new Cozinha();
-        jp.setNome("Japonesa");
-
-        jp = cadastroCozinha.adicionar(jp);
-
-        System.out.printf("%d - %s\n", br.getId(), br.getNome());
-        System.out.printf("%d - %s\n", jp.getId(), jp.getNome());
 
     }
 }
